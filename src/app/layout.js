84f8b2a5e9c1c85b95f2db9cd1738c1dca3c3dc0
Pelumi Oluwategbe <pelumitegbe@gmail.com>
2025-05-29 21:50:16 -0500

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,30 @@ import "lenis/dist/lenis.css";
 
 export const metadata = {
 	title: "Pelumi | Portfolio",
-	description: "Creative developer portfolio",
+	description: "My personal creative portfolio",
+	authors: [{ name: "Pelumi Oluwategbe" }],
+	keywords: [
+		"software",
+		"react",
+		"fullstack",
+		"portfolio",
+		"software engineer",
+		"developer",
+		"sofware developer",
+	],
+	openGraph: {
+		title: "Pelumi Oluwategbe Portfolio",
+		type: "website",
+		description: "My personal creative portfolio",
+		images: [
+			{
+				url: "https://pelumi-tegbe.vercel.app/images/contact.png",
+				width: 1200,
+				height: 630,
+				alt: "Logo Preview",
+			},
+		],
+	},
 	icons: {
 		icon: "/favicon.png",
 		shortcut: "/favicon.png",
